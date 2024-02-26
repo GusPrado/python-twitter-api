ce19fbff0a98d90934e2b02eb278c840d7547574
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-02-26 13:47:08 -0300

    feat: Add Docker compose & refactor main in another files

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 from typing import List
 import uvicorn
-
-from pymongo import MongoClient
 from fastapi import FastAPI
-from pydantic import BaseModel
-from src.services import get_trends
-
-class TrendItem(BaseModel):
-    name: str
-    url: str
-
-BRAZIL_WOE_ID = 23424768
+from src.responses import TrendItem
+from src.services import get_trends, save_trends
 
 app = FastAPI()
 
 @app.get('/trends', response_model=List[TrendItem])
 def get_trends_route():
-    trends = get_trends(woe_id=BRAZIL_WOE_ID)
-
-    return trends
+    return get_trends()
 
 if __name__ == "__main__":
+    trends = get_trends()
+
+    if not trends:
+        save_trends()
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/services.py
+++ src/services.py
@@ -1,8 +1,10 @@
 from typing import Any, Dict, List
-from src.secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 import tweepy
+from src.secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+from src.constants import BRAZIL_WOE_ID
+from src.connections import trends_collection
 
-def get_trends(woe_id: int) -> List[Dict[str, Any]]:
+def _get_trends(woe_id: int, api: tweepy.API) -> List[Dict[str, Any]]:
     """Get treending topics from Twitter API.
 
     Args:
@@ -12,13 +14,36 @@ def get_trends(woe_id: int) -> List[Dict[str, Any]]:
         List[Dict[str, Any]]: Trends list.
     """
 
-    auth = tweepy.Client(consumer_key='5THZPPUSr4iGGzYggbYXTUcOZ', consumer_secret='eJEtAwFrHQVmUuyo7fWBNa3N9dUv1bwQzz59pP5ajkeGRTyDjv', access_token='140272278-vgIp637jceMX7Vrh7LFvAdluRvSYAs3S2mtZlIJ2', access_token_secret='OYXxcGYxTlIMOCquAV14sSzCsQnssjKOoZfYfJqb666zm')
+    auth = tweepy.Client(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
 
     api = tweepy.API(auth)
     
-    trends = api.get_place_trends(id=woe_id)
+    trends = api.get_place_trends(woe_id)
 
     # for tweet in trends:
     #    print(tweet)
 
     return trends[0]['trends']
+
+def get_trends() -> List[Dict[str, Any]]:
+    """Get treending topics persisted in MongoDB.
+
+    Args:
+        woe_id (int): Identifier of location.
+
+    Returns:
+        List[Dict[str, Any]]: Trends list.
+    """
+    trends = trends_collection.find({})
+    return list(trends)
+
+
+def save_trends() -> None:
+    """Get trends topics and save on MongoDB."""
+    auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    api = tweepy.API(auth)
+
+    trends = _get_trends(woe_id=BRAZIL_WOE_ID, api=api)
+    trends_collection.insert_many(trends)
